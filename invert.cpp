511ad6cb2f48ea7e2b29b4ce00e6b98168cb8902
TonyLiu <ltsplice@gmail.com>
2023-03-13 10:39:28 -0400

    i

---
+++ invert.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "imageio.h"
+using namespace std;
+int main(){
+    std::string input = "image1.pgm";
+    int img[MAX_H][MAX_W];
+    int h, w;
+    readImage(input, img, h, w);
+
+    int out[MAX_H][MAX_W];
+    for(int row = 0; row < h; row++) {
+        for(int col = 0; col < w; col++) {
+            out[row][col] = abs(255 - img[row][col]);
+        }
+    }
+    
+    // and save this new image to file "outImage.pgm"
+    writeImage("outImage.pgm",out, h, w);
+    return 0;
+}
